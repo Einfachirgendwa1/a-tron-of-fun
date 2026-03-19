a1c7abaf1ef7e49ed292694b1d90ed91731fd1d7
dascha.petrov <petrafish.blub.blub@gmail.com>
2026-03-19 13:59:56 +0100

    GaLiGrü)

---
+++ LightCyclesWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LightCyclesWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LightCyclesWorld extends World
+{
+
+    /**
+     * Constructor for objects of class LightCyclesWorld.
+     * 
+     */
+    public LightCyclesWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
