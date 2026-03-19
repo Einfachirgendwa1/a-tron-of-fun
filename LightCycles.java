de10079a04c5a73d70b0f41b9c7e90c6dda0e5f5
dascha.petrov <petrafish.blub.blub@gmail.com>
2026-03-19 13:38:41 +0100

    Okeee))

---
+++ LightCycles.java
@@ -0,0 +1,18 @@
+import greenfoot.World;
+
+public class LightCycles extends MiniGame{
+    @Override
+    public Player createPlayer() {
+        return null;
+    }
+
+    @Override
+    public World createWorld() {
+        return null;
+    }
+
+    @Override
+    public void miniGameStart() {
+
+    }
+}
