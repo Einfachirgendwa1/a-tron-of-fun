532ceaea7fcafc6394f7a142b431cbfc9581cfdc
dalex09 <patrafish.blub.blub@gmail.com>
2026-04-16 00:02:51 +0200

    I tried my best. Don't hate me, because ts took 6 hours of my wednesday and also brought me to tears twice. I cried. How are you feeling? I hope you feel great and have not cried recently. lmk twin.

--- LightCyclePlayer.java
+++ LightCyclePlayer.java
@@ -1,5 +1,26 @@
+import greenfoot.Greenfoot;
+
 public class LightCyclePlayer extends Player {
+    private int direction = 1; 
     public void act() {
-        super.act();
+        int result = direction % 4;
+
+        if        (result == 3 || result == -3) {
+            moveUp();
+        } else if (result == 2 || result == -2) {
+            moveRight();
+        } else if (result == 1 || result == -1) {
+            moveDown();
+        } else if (result == 0) {
+            moveLeft();
+        }
+
+        if (Greenfoot.isKeyDown ("a")) {
+            direction = direction - 1;
+        } 
+
+        if (Greenfoot.isKeyDown ("d")) {
+            direction = direction + 1;
+        }
     }
 }
